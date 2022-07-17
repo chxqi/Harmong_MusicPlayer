8bcd81f1c1caa18dd4db427cc704ac383010f899
chxqi <52943946+chxqi@users.noreply.github.com>
2022-07-17 18:20:57 +0800

    Add files via upload
    init

---
+++ MyMusicApplication/entry/src/main/java/com/example/mymusicapplication/slice/MainAbilitySlice.java
@@ -0,0 +1,23 @@
+package com.example.mymusicapplication.slice;
+
+import com.example.mymusicapplication.ResourceTable;
+import ohos.aafwk.ability.AbilitySlice;
+import ohos.aafwk.content.Intent;
+
+public class MainAbilitySlice extends AbilitySlice {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setUIContent(ResourceTable.Layout_login_page);
+    }
+
+    @Override
+    public void onActive() {
+        super.onActive();
+    }
+
+    @Override
+    public void onForeground(Intent intent) {
+        super.onForeground(intent);
+    }
+}
